bd5f202c1f36dce050f1147bd0e9a3e73e45e339
ArielVinis <ariel.franca@hotmail.com>
2024-09-16 21:16:21 -0300

    Calculadora para verificar o valor do produto com desconto.

--- .idea/Main/3_CalculadoraDeDesconto.py
+++ .idea/Main/3_CalculadoraDeDesconto.py
@@ -1,7 +1,7 @@
 # Solicita ao usuário que insira o valor do produto e o percentual de desconto
 
-valor_produto = float(input("Digite o valor do produto: R$ "))
-percentual_desconto = float(input("Digite o percentual de desconto: "))
+valor_produto = float(input("Digite o valor de um produto: R$ "))
+percentual_desconto = float(input("Digite um percentual de desconto: % "))
 
 
 # Verifica se o percentual de desconto está dentro dos limites aceitáveis (0-100%)
