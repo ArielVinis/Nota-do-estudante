8872695cbbc92c6a14927056fb7cd5b965a34abf
ArielVinis <ariel.franca@hotmail.com>
2024-09-18 19:37:16 -0300

    NumPy oferece uma vasta gama de funcionalidades, incluindo arrays multidimensionais e funções sofisticadas. Além disso, disponibiliza ferramentas para integração com código em C/C++ e Fortran, bem como recursos essenciais de álgebra linear, transformada de Fourier e geração de números aleatórios.

---
+++ .idea/Main/3_EstruturaDeDados/3.5_ObjetosTipoArrayNumPy.py
@@ -0,0 +1,26 @@
+# Importe a biblioteca NumPy
+import numpy as np
+
+# Crie um array NumPy de números inteiros
+my_array = np.array([1, 2, 3, 4, 5])
+
+# Imprime o array
+print("Array original:")
+
+print(my_array)
+
+# Realize operações matemáticas com o array
+squared_array = my_array ** 2  # Eleva cada elemento ao quadrado
+
+sum_of_elements = np.sum(my_array)  # Calcula a soma de todos os elementos
+
+# Imprima os resultados
+print("\n Array ao quadrado:")
+print(squared_array)
+
+print("\n Soma dos elementos:")
+print(sum_of_elements)
+
+# Acesse elementos do array
+element_at_index_2 = my_array[2]  # Acessa o elemento no índice 2
+print("\n Elemento no índice 2:", element_at_index_2)
